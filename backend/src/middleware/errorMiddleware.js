4c23a9d9babbe2aca7b034d2f4f64a6eca476103
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-12-03 21:31:26 +0700

    fix(backend): duplicate entry should return 400

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -1,11 +1,12 @@
 export const errorMiddleware = (err, _req, res, _next) => {
-  const statusCode = err.status || 500;
-  const bodyStatus = statusCode <= 500 ? "fail" : "error";
-
   // sql duplicate
   if (err.code === "ER_DUP_ENTRY" || err.errno === 1062) {
+    err.status = 400;
     err.message = "Data already exists";
   }
+
+  const statusCode = err.status || 500;
+  const bodyStatus = statusCode <= 500 ? "fail" : "error";
 
   res.status(statusCode).json({
     status: bodyStatus,
