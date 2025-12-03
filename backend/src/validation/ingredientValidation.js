7c4f90579584b6a8ffe3a9eef5345c30be8957fe
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-12-03 08:53:39 +0700

    feat: use strict on all zod validation model

--- backend/src/validation/ingredientValidation.js
+++ backend/src/validation/ingredientValidation.js
@@ -1,8 +1,10 @@
 import z from "zod";
 
-export const ingredientSchema = z.object({
-  name: z
-    .string("Name must be a string")
-    .min(3, "Name must contain more than 3 characters")
-    .max(64, "Name must not contain more than 64 characters"),
-});
+export const ingredientSchema = z
+  .object({
+    name: z
+      .string("Name must be a string")
+      .min(3, "Name must contain more than 3 characters")
+      .max(64, "Name must not contain more than 64 characters"),
+  })
+  .strict();
--- backend/src/validation/recipeValidation.js
+++ backend/src/validation/recipeValidation.js
@@ -1,10 +1,12 @@
 import z from "zod";
 
-export const recipeSchema = z.object({
-  title: z
-    .string("Title must be a string")
-    .min(3, "Title must contain more than 3 characters")
-    .max(64, "Title must not contain more than 64 characters"),
-  description: z.string("Description must not be empty"),
-  image_url: z.string("Image must be set"),
-});
+export const recipeSchema = z
+  .object({
+    title: z
+      .string("Title must be a string")
+      .min(3, "Title must contain more than 3 characters")
+      .max(64, "Title must not contain more than 64 characters"),
+    description: z.string("Description must not be empty"),
+    image_url: z.string("Image must be set"),
+  })
+  .strict();
