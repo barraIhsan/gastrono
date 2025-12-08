018877cea26898b36145e50ec969b80da7419f19
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-12-08 21:02:41 +0700

    fix(frontend): also add duration on edit

--- frontend/src/app/recipe/[id]/edit/Edit.tsx
+++ frontend/src/app/recipe/[id]/edit/Edit.tsx
@@ -45,6 +45,10 @@ export default function Edit({ id }: { id: string }) {
     },
   });
 
+  const errDuration =
+    form.formState.errors.hours?.message ??
+    form.formState.errors.minutes?.message;
+
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
@@ -55,6 +59,8 @@ export default function Edit({ id }: { id: string }) {
         form.reset({
           title: res.data.title,
           description: JSON.parse(res.data.description),
+          hours: Math.floor(res.data.total_minutes / 60),
+          minutes: res.data.total_minutes % 60,
         });
 
         // set preview img
@@ -93,6 +99,7 @@ export default function Edit({ id }: { id: string }) {
       const payload = {
         title: data.title,
         description: JSON.stringify(data.description),
+        total_minutes: Number(data.hours) * 60 + Number(data.minutes),
         image_url: imageUrl,
       };
 
@@ -202,24 +209,73 @@ export default function Edit({ id }: { id: string }) {
           />
         </FieldGroup>
         <FieldGroup>
-          <Controller
-            name="title"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field>
-                <FieldLabel>Recipe Name</FieldLabel>
-                <Input
-                  {...field}
-                  id={field.name}
-                  aria-invalid={fieldState.invalid}
-                  autoComplete="off"
+          <div className="flex flex-col sm:flex-row gap-5">
+            <Controller
+              name="title"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Recipe Name</FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Field className="w-fit">
+              <FieldLabel>Duration</FieldLabel>
+              <div className="flex items-center gap-2.5">
+                <div className="flex items-center gap-1">
+                  <Controller
+                    name="hours"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Input
+                        {...field}
+                        id={field.name}
+                        aria-invalid={fieldState.invalid}
+                        autoComplete="off"
+                        className="w-11"
+                        placeholder="01"
+                        maxLength={2}
+                      />
+                    )}
+                  />
+                  <p>h</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <Controller
+                    name="minutes"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Input
+                        {...field}
+                        id={field.name}
+                        aria-invalid={fieldState.invalid}
+                        autoComplete="off"
+                        className="w-11"
+                        placeholder="30"
+                        maxLength={2}
+                      />
+                    )}
+                  />
+                  <p>m</p>
+                </div>
+              </div>
+              {errDuration && (
+                <FieldError
+                  errors={[{ message: errDuration }]}
+                  className="w-30!"
                 />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
+              )}
+            </Field>
+          </div>
         </FieldGroup>
         <FieldGroup>
           <Controller
